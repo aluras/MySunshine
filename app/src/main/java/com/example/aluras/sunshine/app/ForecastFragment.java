2083299b9da9707beab92f865a7d826458d04a3d
André Luis <andrelrs80@gmail.com>
2015-05-06 00:14:23 -0300

    Criação do Menu

--- app/src/main/java/com/example/aluras/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/aluras/sunshine/app/ForecastFragment.java
@@ -9,6 +9,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -27,16 +28,31 @@ public  class ForecastFragment extends Fragment {
     }
 
     @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.forecastfragment, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.action_refresh){
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-
-
-
-        Menu menu = MenuInflater.inflate(R.menu.forecastfragment, container);
-
-        rootView.createContextMenu();
 
         ArrayList<String> dados = new ArrayList<String>();
         dados.add("Hoje - Sol - 28º/21º");
@@ -63,7 +79,7 @@ public  class ForecastFragment extends Fragment {
         ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
         listView.setAdapter(mForecastAdapter);
 
-
         return rootView;
     }
+
 }
